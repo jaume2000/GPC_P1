67fc6e48745285714d3c3e63a8a41f19cd4c2183
Jaume Ivars Grimalt <jivagri@upv.edu.es>
2023-09-20 17:57:39 +0200

    creado proyecto

---
+++ js/escena.js
@@ -0,0 +1,11 @@
+/**
+* Escena.js
+* Seminario nº2 de GPC: Pintar una escena básica con transformaciones, animación y modelos importados.
+* @author <jivagri@edu.upv.es>, 2023
+*/
+
+import * as THREE from '../lib/three.module.js'
+import {GLTFLoader} from '../lib/GLTFLoader.js'
+
+// Variablesde consenso. SIEMPRE necesarias
+let renderer, scene, camera
