bb9a9e5e8e60ce07aca7b4bcf883d5743e0e3bb3
jaume2000 <jaumeig95@gmail.com>
2023-10-17 16:13:48 +0200

    trabajo semi hecho

--- js/trabajo.js
+++ js/trabajo.js
@@ -12,28 +12,58 @@ import Stats from '../lib/stats.module.js'
 import {GUI} from "../lib/lil-gui.module.min.js"
 
 
+// Variables de escena
+
+let radio_planeta = 1000
+let earth_rotation = Math.PI/40
+
+let moon_radius = 200
+let moon_angular_velocity = Math.PI/30
+let moon_rotation_velocity = 0
+let planet_wires = 40
+let orbit_wires = 40
+
+let ship_distance = 10000
+let ship_eulers = [0,0,0]
+let ship_euler_velocity = [0,0]
+let ship_rotation = 0
+let velocity = 50
+
+let camera_distance = [200,20,0]
+
+
 // Variablesde consenso. SIEMPRE necesarias
 let renderer, scene, camera
+let clock
 
 //Globales propias
-let robot;
-let base, brazo, antebrazo, mano, pinza_L, pinza_R;
-let cameraControls;
+let planet, moon, moon_geometry
+let ship, ship_end
+let cannons
 let stats
 let effectController
 
-//Top camera
-let topCamera
+// Lista de misiles
+let instanciables = []
+let linear_misil_time = 0
+let linear_misil_velocity = 1000
+let linear_misil_interval_time = 1
 
-const L = 5
+//Keyboard
+let keypress_W = false
+let keypress_A = false
+let keypress_D = false
+let keypress_S = false
+
 //Acciones
 
 init();
 loadScene();
-setupGUI()
 render();
 
 function init(){
+
+    clock = new THREE.Clock()
 
     //Motor de renderer
     renderer = new THREE.WebGLRenderer()
@@ -46,16 +76,10 @@ function init(){
     renderer.setClearColor(new THREE.Color(0,0,0.2))
 
     //Camara
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 10000)
-    cameraControls = new OrbitControls(camera, renderer.domElement)
-    cameraControls.target.set(0,100,0)
-    camera.position.set(150, 200, 150)
-    camera.lookAt(0, 100, 0)
-
-    topCamera = new THREE.OrthographicCamera(-50,50,50,-50,100,800)
-    topCamera.position.set(0, 500, 0)
-    topCamera.up.set(1,0,0)
-    topCamera.lookAt(0,100,0)
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100000)
+    camera.setRotationFromEuler(new THREE.Euler( ...ship_eulers, 'XYZ' ))
+    camera.position.set(ship_distance + camera_distance[0], camera_distance[1] , camera_distance[2])
+    camera.lookAt(0, 0, 0)
 
     //Monitor
 
@@ -65,163 +89,105 @@ function init(){
 
     //Eventos
     window.addEventListener('resize', updateAspectRatio)
+    document.addEventListener('keydown', keyPressed, false)
+    document.addEventListener('keyup', keyRelased, false)
 }
 
 function loadScene(){
+
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load(
+      '/images/2k_stars_milky_way.jpg',
+      () => {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        texture.colorSpace = THREE.SRGBColorSpace;
+        scene.background = texture;
+    });
+
+
+
     //const default_material = new THREE.MeshBasicMaterial({color: new THREE.Color(1,0,0), wireframe: true})
     const default_material = new THREE.MeshNormalMaterial({wireframe:false, flatShading:true})
     const green_material = new THREE.MeshBasicMaterial({color: new THREE.Color(0,1,0), wireframe: true})
-    robot = new THREE.Object3D()
 
-    //Suelo
-    const suelo = new THREE.Mesh(new THREE.PlaneGeometry(400,400, 10,10),default_material)
-    suelo.rotateX(-Math.PI/2)
-    scene.add(suelo)
+    let earthMaterial = createPlanetMaterial("/images/2k_earth_daymap.jpg");
+    planet = new THREE.Mesh(new THREE.SphereGeometry(radio_planeta, planet_wires, planet_wires), earthMaterial)
+    planet.rotateX(21/180*Math.PI)
+    scene.add(planet)
 
-    //Base del robot
-    base = new THREE.Mesh( new THREE.CylinderGeometry(50,50,15, 50), default_material)
-    robot.add(base)
+    let moonMaterial = createPlanetMaterial("/images/2k_moon.jpg");
+    moon = new THREE.Object3D()
+    moon_geometry = new THREE.Mesh(new THREE.SphereGeometry(moon_radius, planet_wires, planet_wires), moonMaterial)
+    moon_geometry.translateX(-ship_distance)
+    moon.add(moon_geometry)
+    scene.add(moon)
 
-    //Brazo
-    brazo = new THREE.Object3D()
-    const brazo_base = new THREE.Mesh( new THREE.CylinderGeometry(20,20,18, 50), default_material)
-    brazo_base.rotateX(-Math.PI/2)
-    brazo.add(brazo_base)
+    //Línea de órbita de la luna
+    /*
+    let vertices = [];
 
-    const brazo_medio = new THREE.Mesh( new THREE.BoxGeometry(18,120,12), default_material)
-    brazo_medio.position.set(0,60,0)
-    brazo.add(brazo_medio)
+    for (let i = 0; i < orbit_wires; i++) {
+        const theta = (i / orbit_wires) * Math.PI * 2;
+        const x = Math.cos(theta) * ship_distance;
+        const z = Math.sin(theta) * ship_distance;
+        vertices.push(x, 0, z);
+    }
     
-    const brazo_fin = new THREE.Mesh( new THREE.SphereGeometry(20,8,8), default_material)
-    brazo_fin.position.set(0,120,0)
-    brazo.add(brazo_fin)
-
-    //Antebrazo
-    antebrazo = new THREE.Object3D()
-
-    antebrazo.position.set(0,120,0)
-
-    const antebrazo_base = new THREE.Mesh( new THREE.CylinderGeometry(22,22, 6, 30), default_material)
-    antebrazo.add(antebrazo_base)
-
-    const separation = 10;
-    const antebrazo_col1 = new THREE.Mesh( new THREE.BoxGeometry(4,80,4), default_material)
-    antebrazo_col1.position.set(separation,40,separation)
-    antebrazo.add(antebrazo_col1)
-    const antebrazo_col2 = new THREE.Mesh( new THREE.BoxGeometry(4,80,4), default_material)
-    antebrazo_col2.position.set(-separation,40,separation)
-    antebrazo.add(antebrazo_col2)
-    const antebrazo_col3 = new THREE.Mesh( new THREE.BoxGeometry(4,80,4), default_material)
-    antebrazo_col3.position.set(separation,40,-separation)
-    antebrazo.add(antebrazo_col3)
-    const antebrazo_col4 = new THREE.Mesh( new THREE.BoxGeometry(4,80,4), default_material)
-    antebrazo_col4.position.set(-separation,40,-separation)
-    antebrazo.add(antebrazo_col4)
-
-    mano = new THREE.Object3D()
-
-    //Mano
-    const antebrazo_fin = new THREE.Mesh( new THREE.CylinderGeometry(15,15, 40, 30), default_material)
-    antebrazo_fin.rotateX(-Math.PI/2)
-    mano.position.set(0,80,0)
-    mano.add(antebrazo_fin)
+    vertices = new Float32Array(vertices);
     
-    //Pinzas
-    const geometry_R = new THREE.BufferGeometry()
-    const positions_R = new Float32Array([
-        -2,0,0,  // 0
-        -2,0,19, // 1
-        0,2,38, // 2
-        0,18,38, // 3
-        -2,20,19, // 4
-        -2,20,0,  // 5
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    
+    // Define los índices para formar triángulos que conecten los puntos de vértice en un patrón circular
+    const indices = [];
+    for (let i = 0; i < orbit_wires; i++) {
+        indices.push(i, (i + 1) % orbit_wires);
+    }
+    
+    geometry.setIndex(indices);
+    
+    const material = new THREE.LineBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.3 });
+    material.linewidth = 10;
+    const circle = new THREE.LineSegments(geometry, material);
+    
+    // Agregar la circunferencia a la escena
+    scene.add(circle);
+    */
 
-        2,0,0,  // 6
-        2,0,19, // 7
-        2,2,38, // 8
-        2,18,38, // 9
-        2,20,19, // 10
-        2,20,0,  // 11
-    ])
+    const orbit_material = new THREE.MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.1 });
 
-    const geometry_L = new THREE.BufferGeometry()
-    const positions_L = new Float32Array([
-        -2,0,0,  // 0
-        -2,0,19, // 1
-        -2,2,38, // 2
-        -2,18,38, // 3
-        -2,20,19, // 4
-        -2,20,0,  // 5
-
-        2,0,0,  // 6
-        2,0,19, // 7
-        0,2,38, // 8
-        0,18,38, // 9
-        2,20,19, // 10
-        2,20,0,  // 11
-    ])
-
-    const indexes = [0,1,4, 0,4,5, 1,2,3, 1,3,4,
-        6,10,7, 6,11,10, 7,9,8, 7,10,9,
-        0,6,1, 6,7,1, 1,7,2, 2,7,8,
-        2,8,3, 9,3,8,
-        0,11,5,
-        3,10,4, 3,9,10, 4,11,5, 4,10,11
-    ]
-
-    geometry_R.setAttribute('position', new THREE.BufferAttribute(positions_R, 3))
-    geometry_R.setIndex(indexes)
-    geometry_L.setAttribute('position', new THREE.BufferAttribute(positions_L, 3))
-
-    geometry_L.setIndex(indexes)
-    pinza_R = new THREE.Mesh(geometry_R, default_material)
-    pinza_L = new THREE.Mesh(geometry_L, default_material)
-    //geometry_R.computeVertexNormals()
-    //geometry_L.computeVertexNormals()
-    pinza_R.rotateY(Math.PI/2)
-    pinza_L.rotateY(Math.PI/2)
-    pinza_R.position.set(5,-10,10)
-    pinza_L.position.set(5,-10,-10)
+    const orbit = new THREE.Mesh(new THREE.TorusGeometry(ship_distance,10,10,100).rotateX(Math.PI/2), orbit_material)
+    scene.add(orbit)
 
 
-    mano.add(pinza_R)
-    mano.add(pinza_L)
+    //Nave espacial
+    ship = new THREE.Object3D()
+    ship_end = new THREE.Object3D()
+    let ship_geometry = new THREE.Mesh(new THREE.BoxGeometry(5,5,5), default_material)
+    ship_end.add(ship_geometry)
+    ship_end.add(camera)
+    ship_end.translateX(ship_distance)
+    camera.position.set(camera_distance[0], camera_distance[1] , camera_distance[2])
+    
+    ship.add(ship_end)
+    ship.setRotationFromEuler(new THREE.Euler( ...ship_eulers, 'XYZ' ))
+    ship_end.setRotationFromEuler(new THREE.Euler(ship_rotation,0,0))
+    
+    
 
-    antebrazo.add(mano)
-
-    brazo.add(antebrazo)
-
-    base.add(brazo)
-
-    robot.add(new THREE.AxesHelper(50))
-
-    robot.position.set(0,7.5,0)
-    scene.add(robot)
+    scene.add(ship)
+    
 
 }
 
-function setupGUI() {
-    effectController = {
-        giro_base: 0, 
-        giro_brazo: 0, 
-        giro_antebrazo_y: 0, 
-        giro_antebrazo_z: 0, 
-        giro_pinza: 0, 
-        separacion_pinza: 20,
-        alambres: false
+function createPlanetMaterial(route) {
 
-    }
+    var earthTexture = THREE.ImageUtils.loadTexture(route);
 
-    const gui = new GUI()
+    var earthMaterial = new THREE.MeshBasicMaterial();
+    earthMaterial.map = earthTexture;
 
-    const h = gui.addFolder("Controles del Robot")
-    h.add(effectController, "giro_base", -180.0, 180.0, 0.5).name("Giro Base")//.listen()
-    h.add(effectController, "giro_brazo", -45.0, 45.0, 0.5).name("Giro Brazo")
-    h.add(effectController, "giro_antebrazo_y", -180.0, 180.0, 0.5).name("Giro Antebrazo Y")
-    h.add(effectController, "giro_antebrazo_z", -90.0, 90.0, 0.5).name("Giro Antebrazo Z")
-    h.add(effectController, "giro_pinza", 0.0, 180.0, 0.5).name("Giro Pinza")
-    h.add(effectController, "separacion_pinza", 4, 30, 1).name("Separación Pinza")
-    
+    return earthMaterial;
 }
 
 function updateAspectRatio(){
@@ -231,16 +197,79 @@ function updateAspectRatio(){
     camera.updateProjectionMatrix()
 }
 
-function update(delta){
+function keyPressed(event){
+    let key = event.key;
+    if (key == 'w'){
+        keypress_W = true
+    }
+    if (key == 's'){
+        keypress_S = true
+    }
+    if (key == 'a'){
+        keypress_A = true
+    }
+    if (key == 'd'){
+        keypress_D = true
+    }
     
-    base.rotation.y = effectController.giro_base / 180 * Math.PI
-    brazo.rotation.z = effectController.giro_brazo / 180 * Math.PI
-    antebrazo.rotation.y = effectController.giro_antebrazo_y / 180 * Math.PI
-    antebrazo.rotation.z = effectController.giro_antebrazo_z / 180 * Math.PI
-    mano.rotation.z = effectController.giro_pinza / 180 * Math.PI
-    pinza_R.position.set(5,-10, -effectController.separacion_pinza/2)
-    pinza_L.position.set(5,-10, effectController.separacion_pinza/2)
+}
 
+function keyRelased(event){
+    let key = event.key;
+    if (key == 'w'){
+        keypress_W = false
+    }
+    if (key == 's'){
+        keypress_S = false
+    }
+    if (key == 'a'){
+        keypress_A = false
+    }
+    if (key == 'd'){
+        keypress_D = false
+    }
+    
+}
+
+
+function update(){
+    let delta = clock.getDelta()
+    
+
+    if (keypress_W){
+        ship_euler_velocity[1] += Math.cos(ship_rotation)*velocity/ship_distance*delta
+        ship_euler_velocity[0] += -Math.sin(ship_rotation)*velocity/ship_distance*delta
+    }
+    if (keypress_S){
+        ship_euler_velocity[1] -= 0.8*ship_euler_velocity[1]*delta
+        ship_euler_velocity[0] -= 0.8*ship_euler_velocity[0]*delta
+
+        if(Math.abs(ship_euler_velocity[0]) < 0.001 || Math.abs(ship_euler_velocity[1]) < 0.001){
+            ship_euler_velocity[0] = 0
+            ship_euler_velocity[1] = 0
+        }
+    }
+    if (keypress_A){
+        ship_rotation+=1*delta
+    }
+    if (keypress_D){
+        ship_rotation-=1*delta
+    }
+    ship.rotateZ(ship_euler_velocity[1])
+    ship.rotateY(ship_euler_velocity[0])
+    moon.rotateY(moon_angular_velocity*delta)
+    moon_geometry.rotateY(moon_rotation_velocity*delta)
+    planet.rotateY(earth_rotation*delta)
+    ship_end.setRotationFromEuler(new THREE.Euler(ship_rotation,0,0))
+
+    instanciables.forEach(i=>i.update(delta))
+
+
+    linear_misil_time+= delta
+    if(linear_misil_time>linear_misil_interval_time){
+        createLinealShoot(Math.random()*Math.PI*2, Math.random()*Math.PI*2, linear_misil_velocity)
+        linear_misil_time=0
+    }
 }
 
 function render(){
@@ -251,7 +280,44 @@ function render(){
 
     renderer.setViewport(0,0, window.innerWidth, window.innerHeight)
     renderer.render(scene, camera)
+}
 
-    renderer.setViewport(0,window.innerHeight-0.25*Math.min(window.innerHeight, window.innerWidth), 0.25*Math.min(window.innerHeight, window.innerWidth), 0.25*Math.min(window.innerHeight, window.innerWidth))
-    renderer.render(scene, topCamera)
+
+
+
+// Videogame objects
+
+class LinearShoot {
+    constructor(ry,rz,velocity){
+        this.ry = ry
+        this.rz = rz
+        this.velocity = velocity
+
+        this.geometry = new THREE.Mesh(new THREE.SphereGeometry(50,50,planet_wires,planet_wires), new THREE.MeshBasicMaterial())
+        this.geometry.rotateY(ry)
+        this.geometry.rotateZ(ry)
+        scene.add(this.geometry)
+        this.position = radio_planeta
+    }
+
+    update(delta){
+        this.position+=this.velocity*delta
+        this.geometry.position.set(this.position,0,0)
+
+        if(this.position > ship_distance + camera_distance){
+            
+            const indiceObjetoAEliminar = instanciables.indexOf(this);
+
+            if (indiceObjetoAEliminar !== -1) {
+                instanciables.splice(indiceObjetoAEliminar, 1);
+            }           
+                scene.remove( this.geometry )
+        }
+    }
+}
+
+function createLinealShoot(ry,rz,velocity){
+
+    const shot = new LinearShoot(ry,rz,velocity)
+    instanciables.push(shot)
 }
